06c5492ea94229d8a418c478890289418aea6c12
Hedge-creator <artiomorlovalex@gmail.com>
2022-05-10 17:34:02 +0300

    Add opencv.py

---
+++ opencv.py
@@ -0,0 +1,23 @@
+import cv2
+def viewImage(image, name_of_window):
+    cv2.namedWindow(name_of_window, cv2.WINDOW_NORMAL)
+    cv2.imshow(name_of_window, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+image_path = 'img.jpg'
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+image = cv2.imread(image_path)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(
+    gray,
+    scaleFactor= 1.1,
+    minNeighbors= 5,
+    minSize=(10, 10)
+)
+faces_detected = "Persons detected: " + format(len(faces))
+print(faces_detected)
+# Рисуем квадраты вокруг лиц
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0), 2)
+viewImage(image,faces_detected)
