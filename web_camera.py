85fdbb652252b1c2dd45bc3584530a845afea6bb
Hedge-creator <artiomorlovalex@gmail.com>
2022-05-21 09:44:06 +0300

    window resize

--- web_camera.py
+++ web_camera.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 #импорт нужной библиотеки
 
 cap = cv2.VideoCapture(0) #подключаемся к камеры, указываем индекс
 cap.set(cv2.CAP_PROP_FPS, 30) # Частота кадров
@@ -6,8 +6,9 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, 600) # Ширина кадров
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480) # Высота кадров
 while True:
     ret, img = cap.read() #читаем с устройсва картинку
+    cv2.namedWindow("web camera", cv2.WINDOW_NORMAL) #меняем размеры окна
     cv2.imshow("web camera", img) #отображаем изображение в указанном окне
-    if cv2.waitKey(10) == 27: # Клавиша Esc
-        break
+    if cv2.waitKey(10) == 27: #при нажатии клавиши esc, программа завершается
+        break 
 cap.release()
 cv2.destroyAllWindows()
